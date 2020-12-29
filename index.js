f80ec43e8967891b7a160bf051c6e64ef52ab762
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-29 00:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your classes here
 class Tree{
+<<<<<<< HEAD
   constructor(species){
     this.species=species
   }
@@ -20,4 +21,20 @@ class Evergreen extends Tree{
     super(species);
     this.name=name;}
   static definition(){return (`${super.definition()} Evergreens keep their leaves all year round.`)}
+=======
+  Constructors(species){
+    this.species=species
+  }
+  static definition(){
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+  }
+  
+}
+class Decidious extends Tree{
+  Constructors(species, name){
+    super(species,name)
+    this.name=name
+  }
+  static definition(){return `${super.definition}`}
+>>>>>>> 96a78cea4fe2e530724130d05e0d0c18fd2a704c
 }
